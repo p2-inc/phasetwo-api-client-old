3d2e83d6283ac9d465e9d893eff53232f296723f
Soma Holiday <soma@somaholiday.com>
2020-06-01 16:36:15 -0700

    Add debug log

--- src/util/request.js
+++ src/util/request.js
@@ -18,6 +18,7 @@ import fetch from 'isomorphic-unfetch';
  *    }
  */
 function request(url, options) {
+  console.log('🌐 request:', { url, options });
   return new Promise((resolve, reject) => {
     fetch(url, options)
       .then(handleResponse)
