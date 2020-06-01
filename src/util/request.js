723acbd2ff51515a8db944e6e183029b95bbd276
Soma Holiday <soma@somaholiday.com>
2020-06-01 16:46:33 -0700

    Revert "Add debug log"
    This reverts commit 3d2e83d6283ac9d465e9d893eff53232f296723f.

--- src/util/request.js
+++ src/util/request.js
@@ -18,7 +18,6 @@ import fetch from 'isomorphic-unfetch';
  *    }
  */
 function request(url, options) {
-  console.log('🌐 request:', { url, options });
   return new Promise((resolve, reject) => {
     fetch(url, options)
       .then(handleResponse)
