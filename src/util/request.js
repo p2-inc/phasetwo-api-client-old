2bb5ba765e60c88fb220c54bcd9f9b9221481fe2
Soma Holiday <soma@somaholiday.com>
2020-06-15 21:24:42 -0700

    Simplify error handling in Account requests

--- src/util/request.js
+++ src/util/request.js
@@ -7,48 +7,20 @@ import fetch from 'isomorphic-unfetch';
  * leading to the development of shims like this one to allow API consumers to handle
  * API errors via .catch()
  *
- * @return {Promise} A promise resolving to:
- *    {
- *      success: true, // or false
- *      status: 200, // or 400, 401, etc
- *      statusText: "",
- *      response: {
- *        // original response from server
- *      }
- *    }
+ * @return {Promise} A promise resolving to the response object on success or an error object on non-200 or failed requests
  */
 function request(url, options) {
-  return new Promise((resolve, reject) => {
-    fetch(url, options)
-      .then(handleResponse)
-      .then((response) => JSON.parse(response))
-      .then((json) => resolve(json))
-      .catch((error) => {
-        try {
-          reject(JSON.parse(error));
-        } catch (e) {
-          reject(error);
-        }
-      });
-  });
+  return fetch(url, options).then(checkStatus);
 }
 
-function handleResponse(response) {
-  return response.json().then((json) => {
-    // Modify response to include status ok, success, and status text
-    const modifiedJson = {
-      success: response.ok,
-      status: response.status,
-      statusText: response.statusText ? response.statusText : json.error || '',
-      response: json,
-    };
-
-    // If request failed, reject and return modified json string as error
-    if (!modifiedJson.success) return Promise.reject(JSON.stringify(modifiedJson));
-
-    // If successful, continue by returning modified json string
-    return JSON.stringify(modifiedJson);
-  });
+function checkStatus(response) {
+  if (response.ok) {
+    return response;
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
 }
 
 export default request;
